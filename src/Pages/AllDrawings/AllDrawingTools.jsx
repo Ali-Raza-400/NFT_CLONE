699a2acd70bbb18ffbccf822811805a960ca012b
Ali <aliraza.12748g@gmail.com>
2022-05-28 18:32:11 +0500

    added

--- src/Pages/AllDrawings/AllDrawingTools.jsx
+++ src/Pages/AllDrawings/AllDrawingTools.jsx
@@ -13,15 +13,24 @@ function AllDrawingTools() {
     <div >
       <div className="container mx-auto">
         <h3 className='text-4xl my-5 ml-24'> All Drawing Tools</h3>
-        <div key={1} className="flex mx-2">
+        <div className="flex mx-2">
           <div className='grid grid-cols-3 md:grid-col-2 gap-3'>
             <div className='mb-5'>
+            <div className="relative">
+                  <img className='rounded-t-lg mb-5' src={pic1} alt="" />
+
+                
+                    <div className="text-black absolute top-36 left-36  bg-yellow-500 rounded">
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                    </div>
+              
+                </div>
             <div className="overflow-hidden  relative">
                   <img className='rounded-t-lg mb-5' src={pic1} alt="" />
 
                   <div>
-                    <div className=" absolute top-36 left-36  rounded">
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+                    <div className="text-black absolute top-36 left-36  bg-yellow-500 rounded">
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                     </div>
                   </div>
                 </div>
@@ -29,17 +38,8 @@ function AllDrawingTools() {
                   <img className='rounded-t-lg mb-5' src={pic1} alt="" />
 
                   <div>
-                    <div className="text-black absolute sm:top-36 sm:left-36  rounded">
-                    <input type="checkbox"  id="vehicle1" name="vehicle1" value="Bike"/>
-                    </div>
-                  </div>
-                </div>
-            <div className="overflow-hidden  relative">
-                  <img className='rounded-t-lg mb-5' src={pic1} alt="" />
-
-                  <div>
-                    <div className="text-black absolute top-36 left-36  rounded">
-                    <input type="checkbox"  id="vehicle1" name="vehicle1" value="Bike"/>
+                    <div className="text-black absolute top-36 left-36  bg-yellow-500 rounded">
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                     </div>
                   </div>
                 </div>
@@ -50,8 +50,8 @@ function AllDrawingTools() {
                   <img className='rounded-t-lg mb-5' src={pic1} alt="" />
 
                   <div>
-                    <div className="text-black absolute top-36 left-36  rounded">
-                    <input type="checkbox"  id="vehicle1" name="vehicle1" value="Bike"/>
+                    <div className="text-black absolute top-36 left-36  bg-yellow-500 rounded">
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                     </div>
                   </div>
                 </div>
@@ -59,8 +59,8 @@ function AllDrawingTools() {
                   <img className='rounded-t-lg mb-5' src={pic1} alt="" />
 
                   <div>
-                    <div className="text-black absolute top-36 left-36  rounded">
-                    <input type="checkbox"  id="vehicle1" name="vehicle1" value="Bike"/>
+                    <div className="text-black absolute top-36 left-36  bg-yellow-500 rounded">
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                     </div>
                   </div>
                 </div>
@@ -68,8 +68,8 @@ function AllDrawingTools() {
                   <img className='rounded-t-lg mb-5' src={pic1} alt="" />
 
                   <div>
-                    <div className="text-black absolute top-36 left-36  rounded">
-                    <input type="checkbox"  id="vehicle1" name="vehicle1" value="Bike"/>
+                    <div className="text-black absolute top-36 left-36 bg-yellow-500 rounded">
+                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                     </div>
                   </div>
                 </div>
